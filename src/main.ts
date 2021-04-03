084dd5dbde85241ed2522aac486d74cd982904f4
Charlie Monnone <charliesnamehere916@gmail.com>
2021-04-03 16:15:10 -0400

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Game from "./Wolfie2D/Loop/Game";
-import default_scene from "./default_scene";
+import MainMenu from "./ui_mockup_scenes/MainMenu";
+import SplashScreen from "./ui_mockup_scenes/SplashScreen";
 
 // The main function is your entrypoint into Wolfie2D. Specify your first scene and any options here.
 (function main(){
@@ -8,15 +9,27 @@ import default_scene from "./default_scene";
 
     // Set up options for our game
     let options = {
-        canvasSize: {x: 1200, y: 800},          // The size of the game
-        clearColor: {r: 0, g: 0, b: 0},   // The color the game clears to
+        canvasSize: {x: 1920, y: 1080},          // The size of the game
+        clearColor: {r: 0.9607, g: 0.9333, b: 0.9137},   // The color the game clears to
+        inputs: [
+            {name: "forward", keys: ["w"]},
+            {name: "backward", keys: ["s"]},
+            {name: "left", keys: ["a"]},
+            {name: "right", keys: ["d"]},
+            {name: "pickup", keys: ["e"]},
+            {name: "drop", keys: ["q"]},
+            {name: "slot1", keys: ["1"]},
+            {name: "slot2", keys: ["2"]}
+        ],
+        useWebGL: true,                        // Tell the game we want to use webgl
+        showDebug: false                       // Whether to show debug messages. You can change this to true if you want
     }
 
     // Create a game with the options specified
     const game = new Game(options);
 
     // Start our game
-    game.start(default_scene, {});
+    game.start(SplashScreen, {});
 })();
 
 function runTests(){};
