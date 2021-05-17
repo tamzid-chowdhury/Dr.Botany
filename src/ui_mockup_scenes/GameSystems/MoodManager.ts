8c3dc46647b86063201d5a7b7498dbf371b06e85
Tamzid Chowdhury <tamzidchowdhury@Tamzid-Chowdhury.local>
2021-05-17 05:37:14 -0400

    mood bar gui

--- src/ui_mockup_scenes/GameSystems/MoodManager.ts
+++ src/ui_mockup_scenes/GameSystems/MoodManager.ts
@@ -220,8 +220,8 @@ export default class MoodManager implements Updateable {
 			this.happyMood += count;
 
 			// GUI stuff 
-			// MathUtils.clamp(this.happyMood, this.minMoodLevel, this.maxMoodLevel);
-			// this.emitter.fireEvent(InGame_GUI_Events.UPDATE_MOOD_BAR, { moodChange: count, type: type})
+			 MathUtils.clamp(this.happyMood, this.minMoodLevel, this.maxMoodLevel);
+			 this.emitter.fireEvent(InGame_GUI_Events.UPDATE_MOOD_BAR, { moodChange: count, type: type})
 
 		}
 		if (type === -1 && this.currentMoodEffect === "No Effects") {
@@ -229,8 +229,8 @@ export default class MoodManager implements Updateable {
 			this.angryMood += count;
 
 			// GUI stuff
-			// MathUtils.clamp(this.angryMood, this.minMoodLevel, this.maxMoodLevel);
-			// this.emitter.fireEvent(InGame_GUI_Events.UPDATE_MOOD_BAR, { moodChange: count, type: type})
+			MathUtils.clamp(this.angryMood, this.minMoodLevel, this.maxMoodLevel);
+			this.emitter.fireEvent(InGame_GUI_Events.UPDATE_MOOD_BAR, { moodChange: count, type: type})
 		}
 		this.updateCurrentMood();
 	}
@@ -241,6 +241,7 @@ export default class MoodManager implements Updateable {
 			this.happyMood = 0;
 			this.currentMood = PlantMoods.ANGRY;
 			this.emitter.fireEvent(InGame_Events.ANGRY_MOOD_REACHED);
+			this.emitter.fireEvent(InGame_GUI_Events.RESET_MOOD_BAR, { type: -1})
 			// this.receiver.unsubscribe(InGame_Events.UPDATE_MOOD);
 			// console.log("ANGRY MOOD REACHED")
 			// GUI counter (timer)
@@ -252,6 +253,7 @@ export default class MoodManager implements Updateable {
 			this.angryMood = 0;
 			this.currentMood = PlantMoods.HAPPY;
 			this.emitter.fireEvent(InGame_Events.HAPPY_MOOD_REACHED);
+			this.emitter.fireEvent(InGame_GUI_Events.RESET_MOOD_BAR, { type: 1})
 			// this.receiver.unsubscribe(InGame_Events.UPDATE_MOOD);
 			// console.log("HAPPY MOOD REACHED")
 			// GUI counter (timer)
