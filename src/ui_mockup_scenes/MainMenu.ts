195a41cde13551f401f6150393382dd500f61a92
Paul Han <paul.han0527@gmail.com>
2021-05-16 20:15:14 -0400

    level select links

--- src/ui_mockup_scenes/MainMenu.ts
+++ src/ui_mockup_scenes/MainMenu.ts
@@ -3,23 +3,19 @@ import Input from "../Wolfie2D/Input/Input";
 import Sprite from "../Wolfie2D/Nodes/Sprites/Sprite";
 import Layer from "../Wolfie2D/Scene/Layer";
 import Scene from "../Wolfie2D/Scene/Scene";
-import { UIEvents, UILayers, WindowEvents } from "./Utils/Enums";
+import { Scenes, UIEvents, UILayers, WindowEvents } from "./Utils/Enums";
 import { MainMenuLayer } from "./Layers/MainMenu/MainMenuLayer";
 import { BackgroundLayer } from "./Layers/MainMenu/BackgroundLayer";
 import { ControlsLayer } from "./Layers/MainMenu/ControlsLayer";
 import { HelpLayer } from "./Layers/MainMenu/HelpLayer";
 import { LevelSelectLayer } from "./Layers/MainMenu/LevelSelectLayer";
-import { SpringLevelLayer } from "./Layers/MainMenu/SpringLevelLayer";
-import { SummerLevelLayer } from "./Layers/MainMenu/SummerLevelLayer";
-import { FallLevelLayer } from "./Layers/MainMenu/FallLevelLayer";
-import { WinterLevelLayer } from "./Layers/MainMenu/WinterLevelLayer";
 import { OptionsLayer } from "./Layers/MainMenu/OptionsLayer";
 import { GameEventType } from "../Wolfie2D/Events/GameEventType";
 import LevelZero from "./Scenes/LevelZero";
 import GameButton from "./Classes/GameButton";
 import * as Tweens from "./Utils/Tweens"
 import { Physics } from "./Utils/PhysicsOptions"
-import Level_Fall_one from "./Scenes/Level_Fall_One";
+
 
 export default class MainMenu extends Scene {
     mainMenuLayer: MainMenuLayer;
@@ -28,11 +24,7 @@ export default class MainMenu extends Scene {
     helpLayer: HelpLayer;
     optionsLayer: OptionsLayer;
     levelSelectLayer: LevelSelectLayer;
-    springLevelLayer: SpringLevelLayer;
-    summerLevelLayer: SummerLevelLayer;
-    fallLevelLayer: FallLevelLayer;
-    winterLevelLayer: WinterLevelLayer;
-    
+
     cursorLayer: Layer;
     cursorLayer2: Layer;
 
@@ -49,7 +41,8 @@ export default class MainMenu extends Scene {
     currentLayer: string = '';
     screenWipe: Sprite;
     toggleEvents: number = 0;
-    
+    nextLevel: string;
+
     loadScene(): void {
         this.load.image("logo", "assets/misc/logo.png");
         this.load.image("background", "assets/misc/canvas.png");
@@ -86,8 +79,8 @@ export default class MainMenu extends Scene {
 
     startScene(): void {
         this.viewport.setZoomLevel(1);
-        this.center = new Vec2(this.viewport.getCanvasSize().x/2, this.viewport.getCanvasSize().y/2);        
-        
+        this.center = new Vec2(this.viewport.getCanvasSize().x / 2, this.viewport.getCanvasSize().y / 2);
+
         this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "temp_music", loop: true, holdReference: true });
         window.onresize = (e: UIEvent) => { this.emitter.fireEvent(WindowEvents.RESIZED, { eventObject: e }) };
 
@@ -96,13 +89,8 @@ export default class MainMenu extends Scene {
         this.optionsLayer = new OptionsLayer(this, this.center, this.defaultFont);
         this.controlsLayer = new ControlsLayer(this, this.center, this.defaultFont);
         this.helpLayer = new HelpLayer(this, this.center, this.defaultFont);
-        
+
         this.levelSelectLayer = new LevelSelectLayer(this, this.center, this.defaultFont);
-        // This is all levels layers
-        this.springLevelLayer = new SpringLevelLayer(this, this.center, this.defaultFont);
-        this.summerLevelLayer = new SummerLevelLayer(this, this.center, this.defaultFont);
-        this.fallLevelLayer = new FallLevelLayer(this, this.center, this.defaultFont);
-        this.winterLevelLayer = new WinterLevelLayer(this, this.center, this.defaultFont);
 
         this.viewport.setCenter(this.center);
 
@@ -138,7 +126,7 @@ export default class MainMenu extends Scene {
     }
 
     setVisibleLayer(layerName: string): void {
-        if(this.currentLayer) {
+        if (this.currentLayer) {
             this.uiLayers.get(this.currentLayer).disable();
 
         }
@@ -180,16 +168,16 @@ export default class MainMenu extends Scene {
             // This stupid fucking label sends 2 events per click so this dumb bit of code is necessary
             if (event.type === UIEvents.CLICKED_TOGGLE) {
                 this.toggleEvents++;
-                if(this.toggleEvents % 2 === 0) {
+                if (this.toggleEvents % 2 === 0) {
                     let id = event.data.get('id');
-                    for(let toggle of this.optionsLayer.toggles) {
-                        if(id === toggle.invisibleLabel.id) {
-                                toggle.toggleFill();
+                    for (let toggle of this.optionsLayer.toggles) {
+                        if (id === toggle.invisibleLabel.id) {
+                            toggle.toggleFill();
                         }
                     }
                 }
-                
-                    
+
+
             }
 
             if (event.type === GameEventType.MOUSE_DOWN) {
@@ -202,105 +190,114 @@ export default class MainMenu extends Scene {
             }
 
             if (event.type === UIEvents.CLICKED_START) {
-				this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
                 this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
-                
+
 
                 this.screenWipe = this.add.sprite("screen_wipe", UILayers.CURSOR);
                 this.screenWipe.imageOffset = new Vec2(0, 0);
-                this.screenWipe.scale = new Vec2(2,1)
-                this.screenWipe.position.set(2*this.screenWipe.size.x, this.screenWipe.size.y/2);
-                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_ZERO));
+                this.screenWipe.scale = new Vec2(2, 1)
+                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.nextLevel = Scenes.LEVEL_ZERO;
+                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_MAIN));
                 this.screenWipe.tweens.play("levelZeroTransition");
             }
-
-            if (event.type === UIEvents.TRANSITION_LEVEL_ZERO) {
-                let sceneOptions = {
-                    physics: Physics
-                }
-                this.sceneManager.changeToScene(LevelZero, {}, sceneOptions);
-
-            }
-
             if (event.type === UIEvents.CLICKED_LEVEL_SELECT) {
-
-
-                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
                 this.levelSelectLayer.playEntryTweens();
                 this.setVisibleLayer(UILayers.LEVEL_SELECT);
-
-
             }
             if (event.type === UIEvents.CLICKED_SPRING) {
                 this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
-                
 
+                this.nextLevel = Scenes.LEVEL_ZERO;
                 this.screenWipe = this.add.sprite("screen_wipe", UILayers.CURSOR);
                 this.screenWipe.imageOffset = new Vec2(0, 0);
-                this.screenWipe.scale = new Vec2(2,1)
-                this.screenWipe.position.set(2*this.screenWipe.size.x, this.screenWipe.size.y/2);
-                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_ZERO));
+                this.screenWipe.scale = new Vec2(2, 1)
+                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_MAIN));
                 this.screenWipe.tweens.play("levelZeroTransition");
-				// this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
-                // this.setVisibleLayer(UILayers.SPRING_LEVELS);
-                // this.springLevelLayer.enbleButtons();
-                // this.levelSelectLayer.disableButtons();
 
             }
 
             if (event.type === UIEvents.CLICKED_SUMMER) {
-				// this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
-                // this.setVisibleLayer(UILayers.SUMMER_LEVELS);
-                // this.summerLevelLayer.enbleButtons();
-                // this.levelSelectLayer.disableButtons();
+                this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
+
+                this.nextLevel = Scenes.LEVEL_SUMMER_ONE;
+                this.screenWipe = this.add.sprite("screen_wipe", UILayers.CURSOR);
+                this.screenWipe.imageOffset = new Vec2(0, 0);
+                this.screenWipe.scale = new Vec2(2, 1)
+                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_MAIN));
+                this.screenWipe.tweens.play("levelZeroTransition");
 
             }
 
             if (event.type === UIEvents.CLICKED_FALL) {
-				// this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
-                // this.setVisibleLayer(UILayers.FALL_LEVELS);
-                // this.fallLevelLayer.enbleButtons();
-                // this.levelSelectLayer.disableButtons();
-                this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
-                
 
+                this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
+
+                this.nextLevel = Scenes.LEVEL_FALL_ONE;
                 this.screenWipe = this.add.sprite("screen_wipe", UILayers.CURSOR);
                 this.screenWipe.imageOffset = new Vec2(0, 0);
-                this.screenWipe.scale = new Vec2(2,1)
-                this.screenWipe.position.set(2*this.screenWipe.size.x, this.screenWipe.size.y/2);
-                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_FALL_ONE));
+                this.screenWipe.scale = new Vec2(2, 1)
+                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_MAIN));
                 this.screenWipe.tweens.play("levelZeroTransition");
-            }
-            if (event.type === UIEvents.TRANSITION_LEVEL_FALL_ONE) {
-                let sceneOptions = {
-                    physics: Physics
-                }
-                this.sceneManager.changeToScene(Level_Fall_one, {}, sceneOptions);
-
             }
 
             if (event.type === UIEvents.CLICKED_WINTER) {
-				// this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
-                // this.setVisibleLayer(UILayers.WINTER_LEVELS);
-                // this.winterLevelLayer.enbleButtons();
-                // this.levelSelectLayer.disableButtons();
+                this.emitter.fireEvent(GameEventType.STOP_SOUND, { key: "temp_music" });
 
+                this.nextLevel = Scenes.LEVEL_WINTER_ONE;
+                this.screenWipe = this.add.sprite("screen_wipe", UILayers.CURSOR);
+                this.screenWipe.imageOffset = new Vec2(0, 0);
+                this.screenWipe.scale = new Vec2(2, 1)
+                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.screenWipe.tweens.add("levelZeroTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL_MAIN));
+                this.screenWipe.tweens.play("levelZeroTransition");
+
+            }
+            if (event.type === UIEvents.TRANSITION_LEVEL_MAIN) {
+                switch (this.nextLevel) {
+                    case Scenes.LEVEL_ZERO:
+                        this.sceneManager.changeToScene(LevelZero, {}, Physics);
+                        break;
+                    case Scenes.LEVEL_SUMMER_ONE:
+                        console.log("clicked summer ")
+                        break;
+
+                    case Scenes.LEVEL_FALL_ONE:
+                        console.log("clicked fall")
+                        break;
+
+                    case Scenes.LEVEL_WINTER_ONE:
+                        console.log("clicked winter")
+                        break;
+
+                    default:
+                        break;
+                }
+                let sceneOptions = {
+                    physics: Physics
+                }
+                this.sceneManager.changeToScene(LevelZero, {}, sceneOptions);
             }
 
             if (event.type === UIEvents.CLICKED_CONTROLS) {
-				this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
                 this.controlsLayer.playEntryTweens();
                 this.setVisibleLayer(UILayers.CONTROLS);
             }
 
             if (event.type === UIEvents.CLICKED_OPTIONS) {
-                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
                 this.optionsLayer.playEntryTweens();
                 this.setVisibleLayer(UILayers.OPTIONS);
             }
 
             if (event.type === UIEvents.CLICKED_HELP) {
-                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
                 this.helpLayer.playEntryTweens();
                 this.setVisibleLayer(UILayers.HELP);
             }
@@ -320,21 +317,21 @@ export default class MainMenu extends Scene {
             }
 
             if (event.type === UIEvents.TRANSITION_SCREEN) {
-                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "button", loop: false, holdReference: true});
-                switch(this.currentLayer) {
-                    case(UILayers.CONTROLS):
+                this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "button", loop: false, holdReference: true });
+                switch (this.currentLayer) {
+                    case (UILayers.CONTROLS):
                         this.controlsLayer.playExitTweens()
                         break;
-                    case(UILayers.OPTIONS):
+                    case (UILayers.OPTIONS):
                         this.optionsLayer.playExitTweens()
                         break;
-                    case(UILayers.HELP):
+                    case (UILayers.HELP):
                         this.helpLayer.playExitTweens()
                         break;
-                    case(UILayers.LEVEL_SELECT):
+                    case (UILayers.LEVEL_SELECT):
                         this.levelSelectLayer.playExitTweens()
                         break;
-                    default: 
+                    default:
                         break;
                 }
             }
