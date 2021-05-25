c70bd0402d712a72334f306cca87e9a1669f74a7
Paul Han <paul.han0527@gmail.com>
2021-05-24 22:47:46 -0400

    new fixed level maps and music

--- dist/assets/tilemaps/WinterLevel/falltiles.tsx
+++ dist/assets/tilemaps/WinterLevel/falltiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="falltiles" tilewidth="16" tileheight="16" tilecount="391" columns="17">
- <image source="falltiles.png" width="272" height="368"/>
+ <image source="../FallLevel/falltiles.png" width="272" height="368"/>
 </tileset>
--- src/ui_mockup_scenes/Scenes/Level_Winter_One.ts
+++ src/ui_mockup_scenes/Scenes/Level_Winter_One.ts
@@ -1,145 +1,160 @@
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
-import { UIEvents,InGame_Events, Scenes } from "../Utils/Enums";
+import { UIEvents, InGame_Events, Scenes } from "../Utils/Enums";
 import GameLevel from "./GameLevel";
 import OrthogonalTilemap from "../../Wolfie2D/Nodes/Tilemaps/OrthogonalTilemap";
 import Receiver from "../../Wolfie2D/Events/Receiver";
 import Timer from "../../Wolfie2D/Timing/Timer";
 import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
 import GrowthManager from "../GameSystems/GrowthManager";
-import * as Tweens from "../Utils/Tweens";
-import ScriptedSequence from "../Classes/ScriptedSequence";
+import * as Tweens  from "../Utils/Tweens";
 import { Physics } from "../Utils/PhysicsOptions";
-import Level_Winter_Two from "./Level_Winter_Two";
+
 import MainMenu from "../MainMenu";
-import PlayerController from "../Controllers/PlayerController";
+import Level_Winter_Two from "./Level_Winter_Two";
 
 export default class Level_Winter_One extends GameLevel {
 
     collidables: OrthogonalTilemap;
     tilemapSize: Vec2;
     lookDirection: Vec2;
-    maxEnemyNumber: number = 10;
-    currentLevel: string = Scenes.LEVEL_WINTER_ONE;
-    // Custom time
-
+    maxEnemyNumber: number = 13;
     moodEffectTimer: Timer = new Timer(10000, null, false);
     moodBarTimer: Timer = new Timer(6000, null, false);
-    levelZeroReceiver: Receiver = new Receiver();
-
+    levelReceiver: Receiver = new Receiver();
+    currentLevel: string = Scenes.LEVEL_WINTER_ONE;
+	pillBottleTimer: Timer;
+	trashLidTimer: Timer;
     pauseExecution: boolean = false;
     loadScene(): void {
         super.loadScene();
-        this.load.tilemap("level_winter_one", "assets/tilemaps/WinterLevels/WinterLevel1.json");
-
-        this.load.audio("background_music", "assets/music/fall_music.mp3")
+        this.load.tilemap("level_winter_one", "assets/tilemaps/WinterLevel/level_winter_one.json"); // this change
+        this.load.audio("background_music", "assets/music/winter_music.mp3")
     }
 
+	unloadScene(): void {
+        super.unloadScene();
+        this.levelReceiver.destroy();
+    }
 
     startScene(): void {
         super.startScene()
         this.emitter.fireEvent(GameEventType.PLAY_SOUND, { key: "background_music", loop: true, holdReference: true });
-        let tilemapLayers = this.add.tilemap("level_winter_one");
+        
+        let tilemapLayers = this.add.tilemap("level_winter_one");  // this change
         for (let layer of tilemapLayers) {
             let obj = layer.getItems()[0];
             if (obj.isCollidable) {
                 this.collidables = <OrthogonalTilemap>obj;
             }
         }
+
         this.tilemapSize = this.collidables.size;
+
+
         //INITIALIZE PLANT BEFORE PLAYER WHEN MAKING YOUR LEVELS 
         super.initPlant(this.collidables.size);
         this.plant.animation.playIfNotAlready("EH", true);
-        super.initPlayer(this.collidables.size);
-        (<PlayerController>this.player._ai).owner.position.set(730,900)
-        this.equipmentManager.spawnEquipment("PillBottle", new Vec2(900, 920))
-        this.equipmentManager.spawnEquipment("TrashLid", new Vec2(940,920))
+		super.initPlayer(this.collidables.size);
         super.initViewport(this.collidables.size);
         super.initGameUI(this.viewport.getHalfSize());
         super.initPauseMenu(this.viewport.getHalfSize());
         super.initGameOverScreen(this.viewport.getHalfSize());
         super.initLevelCompletionScreen(this.viewport.getHalfSize());
-        super.initSpawnerTimer(3000);
+        super.initSpawnerTimer(4000);
         this.viewport.follow(this.player);
-        this.levelZeroReceiver.subscribe(InGame_Events.ANGRY_MOOD_REACHED);
-        this.levelZeroReceiver.subscribe(InGame_Events.HAPPY_MOOD_REACHED);
-        this.subscribeToEvents();
 
-        // CUSTOM NUMBER OF HEALTHPACK , AMMOPACK
-        this.supportManager.addHealthPacks(20);
-        this.supportManager.addAmmoPacks(20);
-        //////////////////////////////////////////////////////////
-        // new GrowthManager(this, materialsToWin : number) : default set to 50 (2% per items)
-        this.growthManager = new GrowthManager(this, 25);
+        this.levelReceiver.subscribe(InGame_Events.ANGRY_MOOD_REACHED);
+        this.levelReceiver.subscribe(InGame_Events.HAPPY_MOOD_REACHED);
+        this.subscribeToEvents();
+        
+
+
+        //we initialized supportmanager in gamelevel but it starts with 0 healthpacks and 0 ammopacks 
+        //we use addHealthPacks and addAmmoPacks to add how many we want for each level. in tutorial level will have 5 each
+        this.supportManager.addHealthPacks(10); 
+        this.supportManager.addAmmoPacks(10);
+
+        this.growthManager = new GrowthManager(this, 18);
         this.spawnerTimer.start();
         this.nextLevel = Scenes.LEVEL_WINTER_TWO;
+		this.trashLidTimer = new Timer(30000, () => {
+			this.equipmentManager.spawnEquipment('TrashLid', new Vec2(this.plant.position.x, this.plant.position.y + 32))
+			this.pillBottleTimer.start();
+		})
+		this.pillBottleTimer =  new Timer(50000, () => {
+			this.equipmentManager.spawnEquipment('PillBottle', new Vec2(this.plant.position.x + 32, this.plant.position.y + 32))
+		})
+		this.trashLidTimer.start();
     }
 
     updateScene(deltaT: number) {
         super.updateScene(deltaT);
         this.growthManager.update(deltaT);
-        if (this.pauseExecution && this.spawnerTimer.isActive() && !this.completionStatus) {
-            this.spawnerTimer.pause();
-            // console.log(this.spawnerTimer.toString());
-        }
-        else if (!this.pauseExecution && this.spawnerTimer.isPaused() && !this.completionStatus) {
-            this.spawnerTimer.continue();
-        }
-        if (this.spawnerTimer.isStopped() && this.maxEnemyNumber >= this.enemyManager.activePool.length && !this.pauseExecution) {
-            this.spawnerTimer.start();
-            this.enemyManager.spawnEnemy(this.player, this.plant);
-        }
-        if (this.completionStatus && !this.finalWaveCleared && this.enemyManager.activePool.length === 0) {
-            this.spawnerTimer.pause();
-            // Change the number of final wave enemies for each level
-            this.finalWave(10);
-            this.finalWaveCleared = true;
+		if (this.pauseExecution && this.spawnerTimer.isActive() && !this.completionStatus) {
+			this.spawnerTimer.pause();
+			// console.log(this.spawnerTimer.toString());
+		}
+		else if (!this.pauseExecution && this.spawnerTimer.isPaused() && !this.completionStatus) {
+			this.spawnerTimer.continue();
+		}
+		if (this.spawnerTimer.isStopped() && this.maxEnemyNumber >= this.enemyManager.activePool.length && !this.pauseExecution) {
+			this.spawnerTimer.start();
+			this.enemyManager.spawnEnemy(this.player, this.plant);
+		}
+		if (this.completionStatus && !this.finalWaveCleared && this.enemyManager.activePool.length === 0) {
+			this.spawnerTimer.pause();
+			// Change the number of final wave enemies for each level
+			this.finalWave(10);
+			this.finalWaveCleared = true;
 
-        }
+		}
 
 
-        else if (this.pauseExecution && this.moodEffectTimer.isActive()) {
+		else if(this.pauseExecution && this.moodEffectTimer.isActive()) {
             this.moodEffectTimer.pause();
         }
-        else if (!this.pauseExecution && this.moodEffectTimer.isPaused()) {
+        else if(!this.pauseExecution && this.moodEffectTimer.isPaused()) {
             this.moodEffectTimer.continue();
         }
 
+        
         if(this.moodEffectTimer.isStopped() && this.moodEffectTimer.hasRun()) {
-
             this.moodEffectTimer.reset();
             this.plant.animation.play("EH");
             this.moodManager.resetEffect(this, this.player.position);
         }
-       
 
 
 
-        while (this.levelZeroReceiver.hasNextEvent()) {
-            let event = this.levelZeroReceiver.getNextEvent();
-            if (event.type === InGame_Events.ANGRY_MOOD_REACHED) {
+        while (this.levelReceiver.hasNextEvent()) {
+            let event = this.levelReceiver.getNextEvent();
+
+			if (event.type === InGame_Events.ANGRY_MOOD_REACHED) {
                 this.moodEffectTimer.start();
                 this.plant.animation.play("ANGRY", true);
                 this.moodManager.applyEffect(this,"downer", Math.floor(Math.random() * this.moodManager.prototypesAngry.length), this.player.position);
                 
             }
-            if (event.type === InGame_Events.HAPPY_MOOD_REACHED) {
 
+            if (event.type === InGame_Events.HAPPY_MOOD_REACHED) {
                 this.moodEffectTimer.start();
                 this.plant.animation.play("HAPPY", true);
                 this.moodManager.applyEffect(this,"upper", Math.floor(Math.random() * this.moodManager.prototypesHappy.length), this.player.position);
                 
             }
+            
+
+            // We gotta check this with each levels
             if (event.type === UIEvents.CLICKED_RESTART) {
                 this.nextLevel = this.currentLevel;
-
                 this.screenWipe.imageOffset = new Vec2(0, 0);
-                this.screenWipe.scale = new Vec2(2, 1)
-                this.screenWipe.position.set(2 * this.screenWipe.size.x, this.screenWipe.size.y / 2);
+                this.screenWipe.scale = new Vec2(2,1)
+                this.screenWipe.position.set(2*this.screenWipe.size.x, this.screenWipe.size.y/2);
                 this.screenWipe.tweens.add("levelTransition", Tweens.slideLeft(this.screenWipe.position.x, 0, 500, UIEvents.TRANSITION_LEVEL));
                 this.screenWipe.tweens.play("levelTransition");
             }
-
-            if (event.type === UIEvents.TRANSITION_LEVEL) {
+            
+			if (event.type === UIEvents.TRANSITION_LEVEL) {
                 let sceneOptions = {
                     physics: Physics
                 }
@@ -159,21 +174,17 @@ export default class Level_Winter_One extends GameLevel {
             }
 
         }
-
+   
 
     }
 
     protected subscribeToEvents() {
-        this.levelZeroReceiver.subscribe([
+        this.levelReceiver.subscribe([
+   
+            UIEvents.CLICKED_RESTART,
+			UIEvents.TRANSITION_LEVEL
 
-            UIEvents.TRANSITION_LEVEL,
-            UIEvents.CLICKED_RESTART
         ]);
     }
 
-    unloadScene(): void {
-        super.unloadScene();
-        this.levelZeroReceiver.destroy();
-        this.load.keepAudio("background_music");
-    }
 }
